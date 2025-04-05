96b02b3c8064554bb7cd333f1fe460aecd724b91
Daniel Park <intheimageofyeshua@gmail.com>
2025-04-05 10:56:27 +0900

    Fix Time Error

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -9,7 +9,7 @@ std::string Logger::CurrentDate() {
   std::time_t now =
       std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 
-  std::string output{30, '\0'};
+  std::string output(30, '\0');
   std::strftime(&output[0], output.size(), "%y-%m-%d %H:%M:%S",
                 std::localtime(&now));
 
@@ -24,7 +24,7 @@ void Logger::Log(const std::experimental::string_view message) {
 void Logger::Error(const std::experimental::string_view message,
                    std::experimental::source_location location) {
   std::cout << RED << "ERROR: " << CurrentDate() << " - " << message
-            << "FILE: " << location.file_name()
-            << "FUNC: " << location.function_name()
-            << "LINE: " << location.line() << RESET << "\n";
+            << " FILE: " << location.file_name()
+            << " FUNC: " << location.function_name()
+            << " LINE: " << location.line() << RESET << "\n";
 }
